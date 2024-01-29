a36753bea6edcb6987af03eca63d601e97647692
k-imy <kimberlysokpoh@hotmail.com>
2024-01-29 15:10:19 +0100

    test git

---
+++ Session-01/index.js
@@ -0,0 +1,35 @@
+// declaration des variables /* les commentaires multilignes */
+
+
+let nombreUn;
+let nombreDeux ;
+let somme;
+
+const PI = 3.14;
+
+nombreUn = 16;
+nombreDeux = 20;
+somme = nombreUn + nombreDeux;
+
+
+//fonction
+
+function greet() {
+
+    console.log("helloooo!" + nom)
+}
+greet (David)
+
+function sum(x, y){
+    return x+y
+}
+
+console.log(sum(12, 30))
+
+function inscrire() {
+    let nom = "AAAA";
+    let prenom = "ali";
+    let age = 19;
+
+    
+}
---
+++ Session-02/index.js
@@ -0,0 +1,35 @@
+let texte1 = "OFPPT";
+texte1 +=" ";
+let texte2= "en force";
+let texte3 = texte1 + texte2;
+
+console.log(texte3);
+
+let a = 10;
+let b = "10";
+let x = a==b;
+let y = a===b;
+
+if(a==b) {
+    let x;
+} else if (a===b) {
+    let y ;
+}; 
+console.log(x, y);
+
+let statut="etudiant";
+let age=20;
+if(statut=="etudiant" || age===20) {
+    console.log("okkkkkkkkkkkkkkk")
+} else if (statut=="employe"){
+    console.log("error")
+};
+
+
+
+let valide=false;
+
+if(!valide){
+    console.log("okay")
+};
+console.log(valide);
